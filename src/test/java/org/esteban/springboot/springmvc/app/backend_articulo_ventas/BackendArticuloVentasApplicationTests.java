ba67dccb523c0afa785200c728cead5e781a5248
Esteban Salazar <estebansalazar1407@gmail.com>
2025-06-05 21:13:32 -0500

    Agregando base de datos temporal para pruebas

--- src/test/java/org/esteban/springboot/springmvc/app/backend_articulo_ventas/BackendArticuloVentasApplicationTests.java
+++ src/test/java/org/esteban/springboot/springmvc/app/backend_articulo_ventas/BackendArticuloVentasApplicationTests.java
@@ -2,7 +2,9 @@ package org.esteban.springboot.springmvc.app.backend_articulo_ventas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class BackendArticuloVentasApplicationTests {
 
